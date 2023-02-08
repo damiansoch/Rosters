6de659c04a0100a7311bac0efe8570c02551a971
Damian Socha <damiansoch@hotmail.com>
2023-02-08 17:30:49 +0000

    home screen table completed

---
+++ client/src/screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RosterTable from "../components/RosterTable";
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <RosterTable />
+    </div>
+  );
+};
+
+export default HomeScreen;
